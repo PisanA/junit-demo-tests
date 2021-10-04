ae6e12219cbacd7e751e6b36835f57bde1c1ba8f
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2021-10-03 19:25:02 -0700

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+// This directory within the tests repository may house solution code to ensure that the tests are working properly. This means that the tests repository will normally be private.
+
+import java.util.*;
+
+class Main {
+  public static int input() {
+    Scanner scan = new Scanner(System.in);
+    return scan.nextInt();
+  }
+  
+  public static void print() {
+    System.out.println("JUnit is so cool!");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
